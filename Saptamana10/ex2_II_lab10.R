f8da4f43f1898a5e2fc62463f44c5df54a6385e4
eLizA2-3 <125999724+eLizA2-3@users.noreply.github.com>
2023-05-02 10:22:42 +0300

    Add files via upload

---
+++ Saptamana10/ex2_II_lab10.R
@@ -0,0 +1,27 @@
+
+LNM_student = function(n, r)
+  {
+  a = rt(n, r)
+  medie_esantion = mean(a) #media esantion
+  ds = sd(a) #deviatie standard esantion
+  medie_as = 0 #medie asteptata
+  ds_as = sqrt(r/(r-2)) #deviatie standard asteptata
+  
+  cat("Media eșantionului:", medie_esantion, "\n")
+  cat("Deviația standard a eșantionului:", ds, "\n")
+  cat("Media așteptată:", medie_as, "\n")
+  cat("Deviația standard așteptată:", ds_as, "\n")
+  
+  if((abs(medie_esantion - medie_as) < ds_as/sqrt(n)) && (abs(ds - ds_as) < ds_as/sqrt(n)))
+    cat("LNM este verificată pentru distribuția Student(",r,") cu n = ", n, "\n\n")
+  else 
+    cat("LNM nu este verificată pentru distribuția Student(",r,") cu n = ", n, "\n\n")
+}
+
+n_val = c(1000, 10000, 100000, 1000000)
+r_val = c(2, 3, 4, 5)
+
+for (r in r_val) 
+  for (n in n_val) 
+    LNM_student(n, r)
+
