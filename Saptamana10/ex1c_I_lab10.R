204b23ce579d6109700d36ace93b8e3b09e00500
eLizA2-3 <125999724+eLizA2-3@users.noreply.github.com>
2023-05-02 10:25:49 +0300

    Update ex1c_I_lab10.R

--- Saptamana10/ex1c_I_lab10.R
+++ Saptamana10/ex1c_I_lab10.R
@@ -4,7 +4,7 @@ densitate_gauss = function(m, sigma, a, n)
   b = seq(0, a, n)
   #distributia normala standard
   y = dnorm(b, m, sigma) 
-  plot(b,y,type="l", col="blue")
+  plot(b,y,type="o", col="blue")
   print(y)
   
 }
