95cbd42e0fced69b4518ef8b0fe873063b3f1410
eLizA2-3 <125999724+eLizA2-3@users.noreply.github.com>
2023-05-16 10:10:07 +0300

    Add files via upload

---
+++ Saptamana12/ex_simulare.R
@@ -0,0 +1,26 @@
+variabila_aleatoare = function(x_var, prob) 
+{
+  # verific daca nr variabilelor coincide cu nr probabilitatilor
+  if (abs(sum(prob) - 1) > 1e-6) {
+    cat("Nr variabilelor e diferit de nr probabilitatilor! Ele vor fi normalizate.\n")
+    prob = prob / sum(prob)
+  }
+  
+  cum_prob = cumsum(prob)
+  r = runif(1)
+  
+  for (i in seq_along(cum_prob)) {
+    if (r < cum_prob[i]) 
+      return(x_var[i])
+  }
+  
+  cat("Nu se poate genera o valoare!")
+  return
+}
+
+# Exemplu de utilizare
+x_var = c(10, 22, 3)
+prob = c(0.1, 0.4, 0.5)
+
+
+print(variabila_aleatoare(x_var, prob))
