7628d58be412836537f4103845d818afa0c50c19
eLizA2-3 <125999724+eLizA2-3@users.noreply.github.com>
2023-05-29 15:47:43 +0300

    Update temaB.R

--- Tema_B/temaB.R
+++ Tema_B/temaB.R
@@ -13,59 +13,60 @@ LNM_func = function(n, p)
 
 
 n_val = c(5000, 10000, 100000, 500000)
-r_val = c(0.2, 0.4, 0.6, 0.8)
+r_val = c(0.2, 0.6, 0.6, 0.8)
 
 for (r in r_val) 
   for (n in n_val) 
     LNM_func(n, r)
 
-#########################################2
 
-TLC_student = function(n, r, N, z) {
-  sum=0
-  medie_vec = c()
-  #Se generează n variabile aleatoare cu distribuție Student
-  vec = rt(n, r)
-  vec_medie = mean(vec)  #media variabilelor aleatoare
+#2
+
+
+Student_TLC = function(n, r, N, z)
+{
+  suma=0
+
+  #calculam variabilele aleatoare
+  vector = rt(n, r)
+  #calculez media variabilelor aleatoare
+  vector_mean = mean(vector)
   
-  #valorile teoretice 
-  media_t = 0
-  var_t = (r / (r - 2)) / n
-  # media si dispersia 
-  vec_medie = mean(vec)
-  vec_var = var(vec)
+  #calculez valorile teoretice 
+  media_teoretica = 0
+  st_deviatia = sqrt(r / (r - 2))
   
-  #calculez limitele  utilizând formulele pentru intervalul de confidență bazat 
-  #pe Teorema Limitei Centrale
-  inf_lim = media_t + z * vec_var / sqrt(n) #limta inferioara
-  sup_lim = media_t - z * vec_var / sqrt(n) #limita superioara
+  #deviatia standard
+  dev_s = sd(vector)
+  
+  #calculam limitele
+  inf_lim  =  media_teoretica + z * st_deviatia / sqrt(n)
+  sup_lim  =  media_teoretica - z * st_deviatia / sqrt(n)
   
   for(i in 1:N) {
-    x_n = mean(vec); #media
-    #daca se afla in interiorul intervalului, se incrementeaza sum
-    if(x_n <= sup_lim&& x_n >= inf_lim) { 
-      sum = sum + 1;
+    vec = mean( vector);
+    if(vec <= sup_lim && vec >= inf_lim) {
+      suma = suma + 1;
     }
   }
   
-  eroare_std = sqrt((r / (r - 2)) / n) #eroarea standard
-  eroare_teor_std = sqrt(var_t)        #eroarea standard teoretica
-  eroare_abs = abs(vec_medie - media_t) #eroarea absoluta
-  eroare_abs_std = abs(eroare_std - eroare_teor_std) #eroarea absoluta standard
-  cat("Eroarea standard = ", eroare_std, "\n")
-  cat("Eroarea absoluta standard = ", eroare_abs_std, "\n")
-  cat("Eroarea medie = ", eroare_abs, "\n")
-  cat("Eroarea teoretica standard = " , eroare_teor_std, "\n")
-}
+  
+  #calculez erorile 
+  cat("TLC pentru distributia Student: ", suma/N, "\n")
+  
+  er_abs = abs(vector_mean - media_teoretica)
+  er_abs_st = abs(dev_s - st_deviatia)
+  
+  cat("Eroarea absoluta standard este ", er_abs_st, "\n")
+  cat("Eroarea absoluta medie este ", er_abs, "\n")
 
-N=c(5000,10000, 20000)
-z=c(-1.5, 0, -1.5)
+}
+N = c(5000,10000, 20000)
+z = c(-1.5, 0, -1.5)
 
 for(i in 1:3)
   for(j in 1:3)
-    TLC_student(2, 50, N[i], z[j])
-
-
+    Student_TLC(2, 50, N[i], z[j])
 
  #3
 #programul aproximează prob de a obține un număr între h și k 
